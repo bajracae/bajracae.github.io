66605a21f9530a07a02be0f65aefe1fa9a8e857c
Aeijan <bajracae@oregonstate.edu>
2021-04-24 21:54:45 -0700

    included more information about my contribution to the project

--- js/typewriter.js
+++ js/typewriter.js
@@ -1,16 +1,16 @@
 // List of sentences
-var _CONTENT = [ 
-	"A College Student",
-	"A Computer Science Major",
-	"A Software Developer",
-	"Learning Web Development",
-	"Learning UI/UX Designing"
+var _CONTENT = [
+  "Student",
+  "CS Major",
+  "Software Developer",
+  "Learning Web Development",
+  "Learning UI/UX Designing",
 ];
 
 // Current sentence being processed
 var _PART = 0;
 
-// Character number of the current sentence being processed 
+// Character number of the current sentence being processed
 var _PART_INDEX = 0;
 
 // Holds the handle returned from setInterval
@@ -19,53 +19,51 @@ var _INTERVAL_VAL;
 // Element that holds the text
 var _ELEMENT = document.querySelector("#text");
 
-// Cursor element 
+// Cursor element
 var _CURSOR = document.querySelector("#cursor");
 
 // Implements typing effect
-function Type() { 
-	// Get substring with 1 characater added
-	var text =  _CONTENT[_PART].substring(0, _PART_INDEX + 1);
-	_ELEMENT.innerHTML = text;
-	_PART_INDEX++;
+function Type() {
+  // Get substring with 1 characater added
+  var text = _CONTENT[_PART].substring(0, _PART_INDEX + 1);
+  _ELEMENT.innerHTML = text;
+  _PART_INDEX++;
 
-	// If full sentence has been displayed then start to delete the sentence after some time
-	if(text === _CONTENT[_PART]) {
-		// Hide the cursor
-		_CURSOR.style.display = 'none';
+  // If full sentence has been displayed then start to delete the sentence after some time
+  if (text === _CONTENT[_PART]) {
+    // Hide the cursor
+    _CURSOR.style.display = "none";
 
-		clearInterval(_INTERVAL_VAL);
-		setTimeout(function() {
-			_INTERVAL_VAL = setInterval(Delete, 50);
-		}, 1000);
-	}
+    clearInterval(_INTERVAL_VAL);
+    setTimeout(function () {
+      _INTERVAL_VAL = setInterval(Delete, 50);
+    }, 1000);
+  }
 }
 
 // Implements deleting effect
 function Delete() {
-	// Get substring with 1 characater deleted
-	var text =  _CONTENT[_PART].substring(0, _PART_INDEX - 1);
-	_ELEMENT.innerHTML = text;
-	_PART_INDEX--;
+  // Get substring with 1 characater deleted
+  var text = _CONTENT[_PART].substring(0, _PART_INDEX - 1);
+  _ELEMENT.innerHTML = text;
+  _PART_INDEX--;
 
-	// If sentence has been deleted then start to display the next sentence
-	if(text === '') {
-		clearInterval(_INTERVAL_VAL);
+  // If sentence has been deleted then start to display the next sentence
+  if (text === "") {
+    clearInterval(_INTERVAL_VAL);
 
-		// If current sentence was last then display the first one, else move to the next
-		if(_PART == (_CONTENT.length - 1))
-			_PART = 0;
-		else
-			_PART++;
-		
-		_PART_INDEX = 0;
+    // If current sentence was last then display the first one, else move to the next
+    if (_PART == _CONTENT.length - 1) _PART = 0;
+    else _PART++;
 
-		// Start to display the next sentence after some time
-		setTimeout(function() {
-			_CURSOR.style.display = 'inline-block';
-			_INTERVAL_VAL = setInterval(Type, 100);
-		}, 200);
-	}
+    _PART_INDEX = 0;
+
+    // Start to display the next sentence after some time
+    setTimeout(function () {
+      _CURSOR.style.display = "inline-block";
+      _INTERVAL_VAL = setInterval(Type, 100);
+    }, 200);
+  }
 }
 
 // Start the typing effect on load
